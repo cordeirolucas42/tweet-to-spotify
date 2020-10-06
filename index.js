e9378cbab2bf804b984b4bbee7ab42c958f5b01f
cordeirolucas42 <cordeirolucas42@hotmail.com>
2020-10-06 09:54:34 -0300

    restart

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ var spotifyApi = new SpotifyWebApi({
     redirectUri: 'https://example.com/callback'
 });
 
-
 var T = new Twit({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token: process.env.ACCESS_TOKEN,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
+
 
 spotifyApi.authorizationCodeGrant(process.env.SPOTIFY_CODE)
 .then(
